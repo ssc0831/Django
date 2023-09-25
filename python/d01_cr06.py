89328c5eca246322ca76b56ea5528304a74b832d
ssc0831 <ssc0831@naver.com>
2023-09-25 16:57:30 +0900

    Python Django

--- python/d01_cr06.py
+++ python/d01_cr06.py
@@ -1,18 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
-import re
+from openpyxl import Workbook
 
 res = requests.get('https://finance.naver.com/')
 soup = BeautifulSoup(res.content, 'html.parser')
-selected_elements1 = soup.select("#container > div.aside > div > div.aside_area.aside_popular > table > tbody > tr")
-selected_elements2 = soup.select("#container > div.aside > div > div.aside_area.aside_popular > table > tbody > tr > td > em > span")
 
-selected_elements = selected_elements1 + selected_elements2
-
-for element in selected_elements:
-    print(element.get_text())
-
+# "#container > div.aside > div > div.aside_area.aside_popular > table > tbody"
+tbody = soup.select_one("#container > div.aside > div > div.aside_area.aside_popular > table > tbody")
+# print(tbody)
+trs = tbody.select('tr')
+# print(trs)
+datas = []
+for i in trs:
+    name = i.select_one('th > a').get_text()
+    curr_price = i.select_one('td').get_text()
+    # ch_direction = i['class'][0]
+    ch_direction = i.select_one('td > img')['alt']
+    # ch_direction = i.select_one('td > em > span').get_text()
+    ch_price = i.select_one('td > span').get_text().strip()
+    datas.append([name, curr_price, ch_direction, ch_price])
+# print(name)
+print(datas)
 print('\n---------------------------------------')
 
-# "#container > div.aside > div > div.aside_area.aside_popular > table > tbody > tr:nth-child(1)"
-# "#container > div.aside > div > div.aside_area.aside_popular > table > tbody > tr:nth-child(1) > td:nth-child(3) > em > span"
+# Workbook 활용하여 Spread Sheet(xlsx) 파일 만들기
+# write_wb = Workbook()
+# write_ws = write_wb.create_sheet('결과')
+# for data in datas:
+#     write_ws.append(data)
+
+# write_wb.save(r'testfinace.xlsx')
