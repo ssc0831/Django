c956b8a8a5a8794394ca3fece7e320c610b7e55f
ssc0831 <ssc0831@naver.com>
2023-09-25 13:45:12 +0900

    Python Django

--- python/d01_re2.py
+++ python/d01_re2.py
@@ -25,3 +25,28 @@ print(a)
 print(set(a)) # set은 값의 중복 허용 하지 않는 자료 구조
 
 print(set(re.findall(char, script101)))
+y = set(re.findall(char, script101))
+print(type(y))
+print('='*30)
+
+# set형태를 list로 전환
+z = list(y)
+print(type(z))
+print(z)
+
+for i in z:
+    print(i[:-1]) # : 부분 제외 호출
+
+
+character = []
+for i in z:
+    character += [i[:-1]]
+print(character)
+
+print('='*30)
+character2 = []
+for i in z:
+    char = re.sub(':', '' ,i)
+    # print(char, end= ' ')
+    character2.append(char)
+print(character2)
