120edcfbd8ca4a6d9304e756036df2e7fe9d1926
ssc0831 <ssc0831@naver.com>
2023-09-25 11:48:55 +0900

    Python Django

--- python/d01_apt_pandas.py
+++ python/d01_apt_pandas.py
@@ -2,7 +2,8 @@
 
 import pandas as pd
 
-df = pd.read_csv('apt_201910.csv', encoding='cp949') # 인코딩 없으면 오류남
+df = pd.read_csv('apt_201910.csv', encoding='cp949', thousands=',')
+# 인코딩 없으면 오류남, 단위표시(',' 으로 표시)
 print(len(df))
 print(df.shape)
 print(df.head())
@@ -33,3 +34,29 @@ print('-'*20)
 print(df['가격'].head())
 # 면적이 130㎡ 넘는 아파트 가격
 print(df.가격[df.면적 > 130])
+
+# 면적이 130㎡가 넘고 가격이 2억 미만인 아파트 가격 출력하기
+print(df.가격[(df.면적 > 130) & (df.가격 < 20000)])
+# 면적이 130㎡가 넘거나 가격이 2억 미만인 아파트 가격 출력하기
+print(df.가격[(df.면적 > 130) | (df.가격 < 20000)])
+
+# 가격 정렬하기
+dfAsc = df.sort_values(by='가격', ascending=False)
+print(dfAsc.가격)
+print('-'*20)
+
+# 6억원을 초과하는 가격으로 거래된 아파트, 가격만 출력
+print(df.가격[df.가격 > 60000])
+
+# df.loc[원하는 행 조건, 원하는 열의 조건]
+# 9억원을 초과하는 가격으로 거래된 단지명(아파트), 가격 출력
+print(df.loc[:, ['단지명', '가격']][df.가격 > 90000])
+
+print('-'*20)
+# 단가 = 가격 / 면적
+# '시군구', '면적' , '단가' 10개만 출력하기
+df['단가'] = df.가격 / df.면적
+# df.단가 = df.가격 / df.면적 
+# 없는 컬럼 오류 
+# df['단가'] = df.가격 / df.면적 써야함
+print(df.loc[:10, ['시군구', '면적', '단가']])
