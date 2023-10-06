eee97d05d25cc49a5a33c08cdf04810b447709e3
ssc0831 <ssc0831@naver.com>
2023-10-06 11:59:46 +0900

    Python Django

--- d02/d02_se02.py
+++ d02/d02_se02.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 driver = wd.Chrome()
 driver.get('https://www.melon.com/chart/index.htm')
 
-# 페이지의 소스 찍기
 page = driver.page_source
-# print(page)
+# print(page) # 페이지의 소스 찍기
 soup = BeautifulSoup(page, 'html.parser')
