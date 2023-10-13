402a3b3df6900acaa07f3b5710b288c19d086b8c
ssc0831 <ssc0831@naver.com>
2023-10-13 09:54:15 +0900

    Python Django

--- python/d02/d02_weather_mysql02.py
+++ python/d02/d02_weather_mysql02.py
@@ -12,3 +12,25 @@ conn = pymysql.connect(host=dbURL, port=dbPort, user=dbUser, passwd=dbPass,
 
 font_name = mpl.font_manager.FontProperties(fname='c:/Windows/fonts/malgun.ttf').get_name()
 mpl.rc('font', family=font_name)
+
+select_data = "select * from forecast where city = '부산'"
+curr = conn.cursor()
+curr.execute(select_data)
+result = curr.fetchall()
+# print(result)
+
+# 날짜, 최고기온, 최저기온
+low = [] # 최저기온
+high = [] # 최고기온
+xdata = [] # 날짜
+for r in result:
+    low.append(int(r[4]))
+    high.append(int(r[5]))
+    xdata.append(r[2])
+
+plt.figure(figsize=(10,6))
+plt.plot(xdata, low, label='최저기온')
+plt.plot(xdata, high, label='최고기온')
+plt.title('부산 기온')
+plt.legend()
+plt.show()
